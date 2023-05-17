b57263c35a50fa2b7dcc45a4cee8cb27b154d018
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2023-05-18 04:54:06 +0700

    FIXING DUPLICATE CLASS KOTLIN.COLLECTIONS IN APP LEVEL GRADLE
    Signed-off-by: Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>

--- app/src/main/java/com/project/glamify/HomeFragment.java
+++ app/src/main/java/com/project/glamify/HomeFragment.java
@@ -1,5 +1,10 @@
 package com.project.glamify;
 
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,9 +12,15 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.renderscript.Allocation;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.material.appbar.MaterialToolbar;
@@ -21,13 +32,12 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        TextView homeText = view.findViewById(R.id.home_text);
-        // do something with homeText
+        GridLayout gridLayout = view.findViewById(R.id.grid_layout);
+
         AppCompatActivity activity = (AppCompatActivity) getActivity();
         MaterialToolbar toolbar = activity.findViewById(R.id.topAppBar);
         toolbar.setTitle("Home Screen");
         return view;
     }
 
-    // other methods
 }
