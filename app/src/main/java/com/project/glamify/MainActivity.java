9c708db8fe6685ad55ac2b6460d74f3097d2960a
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2023-05-24 20:23:58 +0700

    Making LoginActivity and ProfileBaseline and google sign in
    Signed-off-by: Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>

--- app/src/main/java/com/project/glamify/MainActivity.java
+++ app/src/main/java/com/project/glamify/MainActivity.java
@@ -16,6 +16,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
@@ -23,11 +24,14 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.BeginSignInResult;
 import com.google.android.gms.auth.api.identity.Identity;
 import com.google.android.gms.auth.api.identity.SignInClient;
 import com.google.android.gms.auth.api.identity.SignInCredential;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -45,11 +49,38 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration appBarConfiguration;
     private MaterialToolbar topAppBar;
 
-    private SignInClient oneTapClient;
-    private BeginSignInRequest signInRequest;
-
     private static final int REQ_ONE_TAP = 2;  // Can be any integer unique to the Activity.
     private boolean showOneTapUI = true;
+
+    private SignInClient oneTapClient;
+    private BeginSignInRequest signInRequest, signUpRequest;
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        switch (requestCode) {
+//            case REQ_ONE_TAP:
+//                try {
+//                    SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(data);
+//                    String idToken = credential.getGoogleIdToken();
+//                    String username = credential.getId();
+//                    String password = credential.getPassword();
+//                    if (idToken !=  null) {
+//                        // Got an ID token from Google. Use it to authenticate
+//                        // with your backend.
+//                        Log.d(TAG, "Got ID token.");
+//                    } else if (password != null) {
+//                        // Got a saved username and password. Use them to authenticate
+//                        // with your backend.
+//                        Log.d(TAG, "Got password.");
+//                    }
+//                } catch (ApiException e) {
+//                    // ...
+//                }
+//                break;
+//        }
+//    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -70,6 +101,30 @@ public class MainActivity extends AppCompatActivity {
 //                // Automatically sign in when exactly one credential is retrieved.
 //                .setAutoSelectEnabled(true)
 //                .build();
+//
+//        oneTapClient.beginSignIn(signUpRequest)
+//                .addOnSuccessListener(this, new OnSuccessListener<BeginSignInResult>() {
+//                    @Override
+//                    public void onSuccess(BeginSignInResult result) {
+//                        try {
+//                            startIntentSenderForResult(
+//                                    result.getPendingIntent().getIntentSender(), REQ_ONE_TAP,
+//                                    null, 0, 0, 0);
+//                        } catch (IntentSender.SendIntentException e) {
+//                            Log.e(TAG, "Couldn't start One Tap UI: " + e.getLocalizedMessage());
+//                        }
+//                    }
+//                })
+//                .addOnFailureListener(this, new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        // No saved credentials found. Launch the One Tap sign-up flow, or
+//                        // do nothing and continue presenting the signed-out UI.
+//                        Log.d(TAG, e.getLocalizedMessage());
+//                    }
+//                });
+
+
         bottomNavigation = findViewById(R.id.bottom_navigation);
         bottomNavigation.setSelectedItemId(R.id.navHome);
 
@@ -107,33 +162,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        switch (requestCode) {
-//            case REQ_ONE_TAP:
-//                try {
-//                    SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(data);
-//                    String idToken = credential.getGoogleIdToken();
-//                    String username = credential.getId();
-//                    String password = credential.getPassword();
-//                    if (idToken !=  null) {
-//                        // Got an ID token from Google. Use it to authenticate
-//                        // with your backend.
-//                        Log.d(TAG, "Got ID token.");
-//                    } else if (password != null) {
-//                        // Got a saved username and password. Use them to authenticate
-//                        // with your backend.
-//                        Log.d(TAG, "Got password.");
-//                    }
-//                } catch (ApiException e) {
-//                    // ...
-//                }
-//                break;
-//        }
-//    }
-
-
 }
