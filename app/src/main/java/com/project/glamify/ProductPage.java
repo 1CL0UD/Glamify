45a545550cf199dddac89b9ce62d0a09a474fb53
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2023-06-10 13:36:07 +0700

    buy button adds order to firestore
    Signed-off-by: Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>

--- app/src/main/java/com/project/glamify/ProductPage.java
+++ app/src/main/java/com/project/glamify/ProductPage.java
@@ -1,15 +1,40 @@
 package com.project.glamify;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProductPage extends AppCompatActivity  {
 
+    private GoogleSignInOptions gso;
+
+    private GoogleSignInClient gsc;
     private Product product;
     private TextView prodTitle, prodPrice, prodDesc;
+
+    private Button buyBtn;
+    private FirebaseFirestore db;
+    private DocumentReference sendRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +57,55 @@ public class ProductPage extends AppCompatActivity  {
 
             }
 
-
-            // ... set other views with the product data
+            buyBtn = findViewById(R.id.buy_button);
+            buyBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Save data to Firestore
+                    saveProductToFirestore(product);
+                }
+            });
         }
 
 
+    }
+
+    private void saveProductToFirestore(Product product) {
+        // Assuming you have already initialized Firestore in your app
+        db = FirebaseFirestore.getInstance();
+
+        // Create a new document in the "products" collection
+        sendRef = db.collection("order_proc").document();
+
+        // Set the data to be saved
+        Map<String, Object> sendData = new HashMap<>();
+        sendData.put("orderName", product.getProduct_title());
+        sendData.put("image", product.getProduct_image());
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().requestId().build();
+        gsc = GoogleSignIn.getClient(this,gso);
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if(acct!=null){
+            Toast.makeText(ProductPage.this, "Tidak Null", Toast.LENGTH_SHORT).show();
+            String userId = acct.getId();
+            sendData.put("userId", userId);
+        }
+        // Save the data to Firestore
+        sendRef.set(sendData)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Data saved successfully
+//                        Toast.makeText(ProductPage.this, "Product saved to Firestore", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Failed to save data
+                        Toast.makeText(ProductPage.this, "Failed to save product", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     
